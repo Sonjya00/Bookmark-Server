86dcd764d98342b6af1a6b07a121855a45d0a97c
Sonia Gorla <soniagorla@msn.com>
2019-03-06 00:37:43 -0600

    Use PORT from environment.

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-#
 # A *bookmark server* or URI shortener that maintains a mapping (dictionary)
 # between short names and long URIs, checking that each new URI added to the
 # mapping actually works (i.e. returns a 200 OK).
@@ -17,33 +15,11 @@
 #     that short name in its dictionary and redirects to the corresponding
 #     long URI.
 #
-# Your job in this exercise is to finish the server code.
-#
-# Here are the steps you need to complete:
-#
-# 1. Write the CheckURI function, which takes a URI and returns True if a
-#    request to that URI returns a 200 OK, and False otherwise.
-#
-# 2. Write the code inside do_GET that sends a 303 redirect to a known name.
-#
-# 3. Write the code inside do_POST that sends a 400 error if the form fields
-#    are missing.
-#
-# 4. Write the code inside do_POST that sends a 303 redirect to the form
-#    after saving a newly submitted URI.
-#
-# 5. Write the code inside do_POST that sends a 404 error if a URI is not
-#    successfully checked (i.e. if CheckURI returns false).
-#
-# In each step, you'll need to delete a line of code that raises the
-# NotImplementedError exception.  These are there as placeholders in the
-# starter code.
-#
-# After writing each step, restart the server and run test.py to test it.
 
 import http.server
 import requests
 from urllib.parse import unquote, parse_qs
+import os
 
 memory = {}
 
@@ -146,6 +122,8 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             self.wfile.write("Error 404: URI check not successful")
             
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    # importing port number
+    port = int(os.environ.get('PORT', 8000))
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
